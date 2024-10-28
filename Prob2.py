3b486d73b37957599c4533a97e568a6297449a08
ReubenBaltazar <rvbaltazar@willamette.edu>
2024-10-28 09:18:58 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -5,7 +5,30 @@
 ##################################################
 
 def is_magic_square(array:list[list[int]]) -> bool:
-    pass
+    n = len(array)
+    for row in array:
+        if len(row) != n:
+            return False
+    target_sum = sum(array[0])
+
+    for row in array:
+        if sum(row) != target_sum:
+            return False
+
+    for col in range(n):
+        col_sum = sum(array[row][col] for row in range(n))
+        if col_sum != target_sum:
+            return False
+
+    main_diag_sum = sum(array[i][i] for i in range(n))
+    if main_diag_sum != target_sum:
+        return False
+
+    anti_diag_sum = sum(array[i][n - i - 1] for i in range(n))
+    if anti_diag_sum != target_sum:
+        return False
+
+    return True
 
 small = [[8,1,6],[3,5,7],[4,9,2]]
 print(is_magic_square(small))
